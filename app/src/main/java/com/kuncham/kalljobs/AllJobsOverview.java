b5c86e8e4f0b9514ad9bec48806395312a9c53fc
admin <cdastagirireddy@hotmail.com>
2018-03-08 17:09:41 +0530

    alljobs overview activity

---
+++ app/src/main/java/com/kuncham/kalljobs/AllJobsOverview.java
@@ -0,0 +1,13 @@
+package com.kuncham.kalljobs;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AllJobsOverview extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_all_jobs_overview);
+    }
+}
