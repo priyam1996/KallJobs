102bd419f9599968170e2177ba48da8afaf40a3b
admin <cdastagirireddy@hotmail.com>
2018-03-08 17:10:29 +0530

    alljobs class

---
+++ app/src/main/java/com/kuncham/kalljobs/Alljobs.java
@@ -0,0 +1,52 @@
+package com.kuncham.kalljobs;
+
+import java.io.Serializable;
+
+/**
+ * Created by admin on 3/8/2018.
+ */
+
+public class Alljobs implements Serializable{
+
+    String headAlljobs;
+    String imageAlljobs;
+    String contentALljobs;
+    String readMore;
+
+    public String getHeadAlljobs() {
+        return headAlljobs;
+    }
+
+    public void setHeadAlljobs(String headAlljobs) {
+        this.headAlljobs = headAlljobs;
+    }
+
+    public String getImageAlljobs() {
+        return imageAlljobs;
+    }
+
+    public void setImageAlljobs(String imageAlljobs) {
+        this.imageAlljobs = imageAlljobs;
+    }
+
+    public String getContentALljobs() {
+        return contentALljobs;
+    }
+
+    public void setContentALljobs(String contentALljobs) {
+        this.contentALljobs = contentALljobs;
+    }
+
+    public String getReadMore() {
+        return readMore;
+    }
+
+    public void setReadMore(String readMore) {
+        this.readMore = readMore;
+    }
+
+
+
+
+
+}
