fd2e84c40e773cfdb9b56e8be8a0c3391083f4c7
priyam1996 <priyankareddythummala@gmail.com>
2018-03-08 17:13:43 +0530

    jobs

---
+++ app/src/main/java/com/kuncham/kalljobs/Jobs.java
@@ -0,0 +1,20 @@
+package com.kuncham.kalljobs;
+
+/**
+ * Created by abc on 3/8/2018.
+ */
+
+public class Jobs {
+
+    String jobName;
+    public String getJobName() {
+        return jobName;
+    }
+
+    public void setJobName(String jobName) {
+        this.jobName = jobName;
+    }
+
+
+
+}
