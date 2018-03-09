76168defd2ce8deea0657720a22ea72260aeac30
priyam1996 <priyankareddythummala@gmail.com>
2018-03-09 10:06:38 +0530

    homepage

--- app/src/main/java/com/kuncham/kalljobs/HomePage.java
+++ app/src/main/java/com/kuncham/kalljobs/HomePage.java
@@ -13,16 +13,42 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.Serializable;
+import java.util.List;
+import java.util.Vector;
 
 public class HomePage extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+    ListView lv;
+    List<Jobs> jobsList;
+    MyAdapter ma;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+
+        jobsList=new Vector<Jobs>();
+        lv=(ListView)findViewById(R.id.govt_lv);
+        ma = new MyAdapter(this,jobsList,R.layout.list);
+        lv.setAdapter(ma);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -41,6 +67,8 @@ public class HomePage extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        load(null);
     }
 
     @Override
@@ -107,5 +135,63 @@ public class HomePage extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    public void load(String s) {
+
+        String url = "http://api.geonames.org/wikipediaSearchJSON?formatted=true&q=%20%22&maxRows=10&username=srik784&style=full";
+
+        JsonObjectRequest jsObjRequest = new JsonObjectRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        jobsList.clear();
+
+                        try {
+                            JSONArray ja = response.getJSONArray("geonames");
+                            for (int i = 0; i < ja.length(); i++) {
+                                try {
+
+
+                                    JSONObject jas = ja.getJSONObject(i);
+
+
+                                    Jobs p = new Jobs();
+                                    p.setJobName(jas.getString("title"));
+                                    jobsList.add(p);
+                                } catch (Exception e) {
+                                    e.printStackTrace();
+
+                                }
+                            }
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+
+                      ma.notifyDataSetChanged();
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // TODO Auto-generated method stub
+
+                        Toast.makeText(HomePage.this, "Try again later", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+        Volley.newRequestQueue(this).add(jsObjRequest);
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent it = new Intent(HomePage.this, AllJobsOverview.class);
+                it.putExtra("p", (Serializable) ma.getItem(i));
+                startActivity(it);
+            }
+        });
     }
 }
