25b80cdfad9bbd9671e1a9017b2685d691ab6df7
admin <cdastagirireddy@hotmail.com>
2018-03-09 10:56:17 +0530

    home page

--- app/src/main/java/com/kuncham/kalljobs/HomePage.java
+++ app/src/main/java/com/kuncham/kalljobs/HomePage.java
@@ -22,6 +22,7 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+import com.kuncham.kalljobs.Adapters.MyAdapter;
 
 import org.json.JSONArray;
 import org.json.JSONException;
